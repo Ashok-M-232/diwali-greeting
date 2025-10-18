e34c4b71879f0811aab2cf8727b8c3b98472cb94
unknown <202g5a0232@gmail.com>
2025-10-18 12:52:33 +0530

    major updates4

--- js/moon.js
+++ js/moon.js
@@ -1,18 +1,18 @@
+// // function createMoon(){
+// //   const container = document.getElementById("moon-container");
+// //   const moon = document.createElement("div");
+// //   moon.className="moon";
+// //   container.appendChild(moon);
+// // }
+
+// // createMoon();
+
 // function createMoon(){
 //   const container = document.getElementById("moon-container");
+//   container.innerHTML = ''; // clear previous moon
 //   const moon = document.createElement("div");
-//   moon.className="moon";
+//   moon.className = "moon";
 //   container.appendChild(moon);
 // }
 
 // createMoon();
-
-function createMoon(){
-  const container = document.getElementById("moon-container");
-  container.innerHTML = ''; // clear previous moon
-  const moon = document.createElement("div");
-  moon.className = "moon";
-  container.appendChild(moon);
-}
-
-createMoon();
